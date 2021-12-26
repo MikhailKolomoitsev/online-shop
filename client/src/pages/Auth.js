3381d3b694c222eb82fd6d54c4a264220b2e8c19
mihon.testosterone <mihon.testosterone@gmail.com>
2021-12-26 22:15:09 +0200

    one more

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,13 +1,26 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Form, Card } from 'react-bootstrap'
 
 const Auth = () => {
     return (
         <Container
             className="d-flex justify-content-center align-items-center"
-            style={{height: window.innerHeight-54}}
+            style={{ height: window.innerHeight - 54 }}
         >
-            AUTH
+            <Card style={{ width: 600 }} className='p-5'>
+                <h2 className='m-auto'>Authorization form</h2>
+                <Form className="d-flex flex-column">
+                    <Form.Control
+                        className='mt-2'
+                        placeholder='Insert e-mail'
+                    />
+                    <Form.Control
+                        className='mt-2'
+                        placeholder='Insert e-mail'
+                    />
+                </Form>
+            </Card>
+
         </Container>
     )
 }
